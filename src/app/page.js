3dceab091b0aecf3f17bd20e14524e00d4f995f4
Dario Hidalgo <dagahi72@gmail.com>
2026-04-15 18:27:46 -0300

    fix links vuelos

--- src/app/page.js
+++ src/app/page.js
@@ -5,15 +5,15 @@
 
   <div style={{display: "flex", gap: "10px", justifyContent: "center"}}>
     
-    <button onClick={() => window.open("https://www.skyscanner.com/transport/flights/bue/mad/", "_blank")}>
+    <button onClick={() => window.open("https://www.skyscanner.com.ar/transport/flights/bue/mad/?adultsv2=1&cabinclass=economy", "_blank")}>
       Madrid 🇪🇸
     </button>
 
-    <button onClick={() => window.open("https://www.skyscanner.com/transport/flights/bue/rio/", "_blank")}>
+    <button onClick={() => window.open("https://www.skyscanner.com.ar/transport/flights/bue/rio/?adultsv2=1&cabinclass=economy", "_blank")}>
       Río 🇧🇷
     </button>
 
-    <button onClick={() => window.open("https://www.skyscanner.com/transport/flights/bue/mia/", "_blank")}>
+    <button onClick={() => window.open("https://www.skyscanner.com.ar/transport/flights/bue/mia/?adultsv2=1&cabinclass=economy", "_blank")}>
       Miami 🇺🇸
     </button>
 
