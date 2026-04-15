2af77c5f24bc5caba1fdd3641adc2033b87410ad
Dario Hidalgo <dagahi72@gmail.com>
2026-04-15 20:18:05 -0300

    crear rutas dinamicas real

--- src/app/vuelos/[destino]/page.js
+++ src/app/vuelos/[destino]/page.js
@@ -4,10 +4,8 @@ export default function Page({ params }) {
   const destino = params.destino;
 
   return (
-    <main style={{padding: "40px"}}>
-      <h1>Vuelos baratos a {destino}</h1>
-
-      <p>Encontrá vuelos al mejor precio</p>
+    <main>
+      <h1>Vuelos a {destino}</h1>
 
       <button onClick={() => 
         window.open(`https://www.google.com/search?q=vuelos+a+${destino}`, "_blank")
