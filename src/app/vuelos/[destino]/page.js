8cf2bc1d0f031dd4f4a4d266795d9f7d93cee2e3
Dario Hidalgo <dagahi72@gmail.com>
2026-04-16 17:56:27 -0300

    test deploy

--- src/app/vuelos/[destino]/page.js
+++ src/app/vuelos/[destino]/page.js
@@ -1,15 +1,3 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-
 export default function Page() {
-  const pathname = usePathname();
-
-  const destino = pathname.split("/vuelos/")[1];
-
-  return (
-    <main>
-      <h1>Vuelos a {destino}</h1>
-    </main>
-  );
+  return <h1>FUNCIONA NUEVA VERSION</h1>;
 }
