a7e8bf145ede2e2e4402a704649d3540d849d861
Dario Hidalgo <dagahi72@gmail.com>
2026-04-16 17:01:31 -0300

    fix home final real

--- src/app/vuelos/[destino]/page.js
+++ src/app/vuelos/[destino]/page.js
@@ -1,10 +1,3 @@
-"use client";
-
 export default function Home() {
-  return (
-    <main>
-      <h1>✈️ Govuelia</h1>
-      <a href="/vuelos/madrid">Buscar vuelos</a>
-    </main>
-  );
+  return <h1>HOME FUNCIONANDO</h1>;
 }
