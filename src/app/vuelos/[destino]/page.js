40804b7e6c81d178b7cb67e6063a655cf13336cd
Dario Hidalgo <dagahi72@gmail.com>
2026-04-16 15:27:44 -0300

    crear home

--- src/app/vuelos/[destino]/page.js
+++ src/app/vuelos/[destino]/page.js
@@ -1,7 +1,36 @@
-export default function Page({ params }) {
+"use client";
+
+export default function Home() {
   return (
-    <main>
-      <h1>Vuelos a {params.destino}</h1>
+    <main style={{
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      background: "linear-gradient(to right, #0f172a, #1e293b)",
+      color: "white",
+      fontFamily: "Arial"
+    }}>
+      <h1 style={{fontSize: "48px"}}>✈️ Govuelia</h1>
+
+      <p style={{marginTop: "10px"}}>
+        Encontrá vuelos baratos al mejor precio
+      </p>
+
+      <a href="/vuelos/madrid">
+        <button style={{
+          marginTop: "20px",
+          padding: "14px 28px",
+          background: "#3b82f6",
+          border: "none",
+          borderRadius: "8px",
+          color: "white",
+          cursor: "pointer"
+        }}>
+          🔍 Buscar vuelos
+        </button>
+      </a>
     </main>
   );
 }
