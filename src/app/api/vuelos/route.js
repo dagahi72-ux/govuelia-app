76fc0e871f38307d40db578e31b7c5fe94aaf363
Dario Hidalgo <dagahi72@gmail.com>
2026-05-04 17:56:42 -0300

    test deploy real final

--- src/app/api/vuelos/route.js
+++ src/app/api/vuelos/route.js
@@ -1,31 +1,7 @@
-export const revalidate = 0;
-
 export const dynamic = "force-dynamic";
 
-export async function GET(req) {
-  const { searchParams } = new URL(req.url);
-
-  const origen = searchParams.get("origen");
-  const destino = searchParams.get("destino");
-  const fecha = searchParams.get("ida");
-
+export async function GET() {
   return Response.json({
-    vuelos: generarVuelosFake(origen, destino, fecha),
-    source: "fake"
+    mensaje: "SI VES ESTO, YA FUNCIONA"
   });
-}
-
-function generarVuelosFake(origen, destino, fecha) {
-  const aerolineas = ["Iberia", "Air Europa", "LATAM", "Level"];
-
-  return Array.from({ length: 5 }).map((_, i) => ({
-    id: i + 1,
-    origen,
-    destino,
-    fecha,
-    aerolinea: aerolineas[Math.floor(Math.random() * aerolineas.length)],
-    precio: 700 + Math.floor(Math.random() * 300),
-    duracion: `${10 + Math.floor(Math.random() * 3)}h`,
-    escalas: Math.random() > 0.5 ? 0 : 1,
-  }));
 }
