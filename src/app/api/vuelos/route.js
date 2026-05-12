558e05f7477cbdc8c370d678d595ebbbf1005ffb
Dario Hidalgo <dagahi72@gmail.com>
2026-05-12 11:11:44 -0300

    replace vuelos api completely

--- src/app/api/vuelos/route.js
+++ src/app/api/vuelos/route.js
@@ -7,33 +7,31 @@ export async function GET(req) {
   const destino = searchParams.get("destino") || "MAD";
   const fecha = searchParams.get("ida") || "2026-05-04";
 
-  const vuelos = [
-    {
-      id: 1,
-      aerolinea: "Iberia",
-      vuelo: "IB6844",
-      salida: origen,
-      llegada: destino,
-      horaSalida: "22:30",
-      horaLlegada: "14:10",
-      precio: 899,
-      fecha,
-    },
-    {
-      id: 2,
-      aerolinea: "LATAM",
-      vuelo: "LA705",
-      salida: origen,
-      llegada: destino,
-      horaSalida: "18:45",
-      horaLlegada: "09:20",
-      precio: 940,
-      fecha,
-    },
-  ];
-
   return Response.json({
-    vuelos,
+    vuelos: [
+      {
+        id: 1,
+        aerolinea: "Iberia",
+        vuelo: "IB6844",
+        salida: origen,
+        llegada: destino,
+        horaSalida: "22:30",
+        horaLlegada: "14:10",
+        precio: 899,
+        fecha,
+      },
+      {
+        id: 2,
+        aerolinea: "LATAM",
+        vuelo: "LA705",
+        salida: origen,
+        llegada: destino,
+        horaSalida: "18:45",
+        horaLlegada: "09:20",
+        precio: 940,
+        fecha,
+      },
+    ],
     source: "FIXED"
   });
 }
