e482a50303179bc18d99d8442ca11e3163950203
Dario Hidalgo <dagahi72@gmail.com>
2026-06-01 16:37:44 -0300

    install travelpayouts drive

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,23 @@
 import "./globals.css";
 import Providers from "./providers";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <Script id="travelpayouts-drive" strategy="afterInteractive">
+          {`
+            (function () {
+              var script = document.createElement("script");
+              script.async = 1;
+              script.src = "https://tp-em.com/NTM1Mjc1.js?t=535275";
+              document.head.appendChild(script);
+            })();
+          `}
+        </Script>
+      </head>
+
       <body>
         <Providers>{children}</Providers>
       </body>
